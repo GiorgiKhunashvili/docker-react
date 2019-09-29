2797fe9e5fd1a6f63c3920f72d546634be3bccb7
giorgi <giorgi.khunashvili.1@btu.edu.ge>
2019-09-30 01:35:44 +0400

    change text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TOLMA 
+         TOLMA ON FEATURE BRANCH
         </p>
         {/* <a
           className="App-link"
